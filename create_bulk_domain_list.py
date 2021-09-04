0a6f1f476a8bbbac05ff5de4995b4f524afff41e
Toby4213 <tobias42@pm.me>
2021-09-04 23:21:39 +0200

    changed domain argument to -t and --tld and added -d --delimiter argument for domain delimiter, changed tld in for loop hell to current_tld

--- create_bulk_domain_list.py
+++ create_bulk_domain_list.py
@@ -2,13 +2,13 @@
 
 import sys, getopt, re
 
-help = "Usage: "+ __file__ + """ [-s bulksize] [-o output] [-d domains]
-\t\t\t\t\t\t\t\t\t[-a|b|c string] [--xa|xb|xc string]
+help = "Usage: "+ __file__ + """ [-s bulksize] [-o output] [-t TLDs]
+\t\t\t\t\t[-d "delimiter"] [-a|b|c string] [--xa|xb|xc string]
 Examples:
-"""+__file__+""" -o \"./out\" -s 2000
-"""+__file__+""" -o \"./out\" -a d -b dsi -c dst -d at,eu,com,net,org
-"""+__file__+""" -o \"./out\" -s 2000 -a d --xb xyz --xc xyz -d at,eu,gmbh
-"""+__file__+""" --output \"./out\" --bulk-size 100 --domain at,eu,gmbh
+"""+__file__+""" -o \"./out\" -s 2000 -d ","
+"""+__file__+""" -o \"./out\" -a d -b dsi -c dst -t at,eu,com,net,org
+"""+__file__+""" -o \"./out\" -s 2000 -a d --xb xyz --xc xyz -t at,eu,gmbh
+"""+__file__+""" --output \"./out\" --bulk-size 100 --tld at,eu,gmbh
 
 Info:
 \tDomain scheme: abc.tld
@@ -17,11 +17,12 @@ Info:
 
 Options:
 \t-h, --help
-\t-s, --bulk-size <size>\tHow many domain per file, default: 0
-\t-o, --output <file>\tOutput file name
-\t-d, --domain <domains>\t comma seperated list of TLDs, default: at
-\t-a|b|c <string>\treplace the default alphabet with selected characters
-\t--xa|xb|xc <string>\tremove the characters in string from alphabet"""
+\t-s, --bulk-size <size>\t\tHow many domains per file, default: 0 (one large file)
+\t-o, --output <file>\t\t\tOutput file name
+\t-t, --tld <domains>\t\t\tComma seperated list of TLDs, default: at
+\t-d, --delimiter <string>\tDelimiter for domains, default: \\n
+\t-a|b|c <string>\t\t\t\treplace the default alphabet with selected characters
+\t--xa|xb|xc <string>\t\t\tremove the characters in string from alphabet"""
 
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h',
             'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p',
@@ -31,7 +32,8 @@ letter["a"] = alphabet.copy()
 letter["b"] = alphabet.copy()
 letter["c"] = alphabet.copy()
 alphamod = {"a": False, "b": False, "c": False}
-domain = ["at"]
+tld = ["at"]
+delimiter = "\n"
 outfile = "./out"
 bulksize = 0
 
@@ -45,9 +47,9 @@ def help_exit(error=""):
 		sys.exit(1)
 
 def main(argv):
-	global letter, domain, outfile, bulksize
+	global letter, tld, outfile, bulksize
 	try:
-		opts, args = getopt.getopt(argv,"hs:o:d:a:b:c:",["help","bulk-size=", "output=", "domain=", "xa=", "xb=", "xc="])
+		opts, args = getopt.getopt(argv,"hs:o:t:d:a:b:c:",["help","bulk-size=", "output=", "tld=", "xa=", "xb=", "xc="])
 	except getopt.GetoptError:
 		help_exit()
 	for opt, arg in opts:
@@ -57,11 +59,15 @@ def main(argv):
 			bulksize = int(arg)
 		elif opt in ("-o", "--output"):
 			outfile = arg
-		elif opt in ("-d", "--domain"):
+		elif opt in ("-t", "--tld"):
 			if re.match("([a-z]){2,}(,([a-z]){2,})*", arg) == None:
-				print(help_exit("Wrong Domain format: \'"+arg+"\'"))
+				print(help_exit("Wrong tld format: \'"+arg+"\'"))
 				exit(1)
-			domain = arg.split(",")
+			tld = arg.split(",")
+		elif opt in ("-d", "--delimiter"):
+			if length(arg) > 2:
+				help_exit("Wrong delimiter, larger than 2 characters: \'"+arg+"\'")
+			delimiter = opt
 		elif opt in ("-a", "-b", "-c"):
 			if alphamod[opt[-1]] == True:
 				help_exit("Only one replacement or exclusion list is allowed per position, a b or c!")
@@ -82,7 +88,7 @@ def main(argv):
 		file = None
 	counter = 0
 	file_count = 0;
-	for tld in domain:
+	for current_tld in tld:
 		for a in letter["a"]:
 			for b in letter["b"]:
 				for c in letter["c"]:
@@ -92,7 +98,7 @@ def main(argv):
 						file = open(outfile+str(file_count), 'w')
 						file_count += 1
 					counter += 1
-					file.write(a+b+c+"."+tld+"\n")
+					file.write(a+b+c+"."+current_tld+delimiter)
 
 
 if __name__ == "__main__":
